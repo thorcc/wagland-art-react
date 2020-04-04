54ce68899c99c2316ae15b60e553b8cb56520026
thorcc <thorchrcow@gmail.com>
2020-04-04 18:53:27 +0200

    inital files

---
+++ src/components/App/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Art from '../Art';
+
+const App = () => {
+
+    return(
+        <div>
+            <h1>Roger Wagland</h1>
+
+            <Art />
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/Art/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Art = () => {
+
+    return(
+        <h3>Art</h3>
+    )
+}
+
+export default Art;
