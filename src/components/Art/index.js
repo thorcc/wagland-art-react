da088aa734862a192a823b21de013047b4835dc8
thorcc <thorchrcow@gmail.com>
2020-05-10 12:46:47 +0200

    add artpage

--- src/components/Art/index.js
+++ src/components/Art/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { withFirebase } from '../Firebase';
+import classes from './style.module.css';
 
 const Art = props => {
+    const [paintings, setPaintings] = useState([]);
+
+    useEffect(() => {  
+
+        const getPaintings = async () => {
+          const res = await props.firebase.paintings.get();
+          const data = res.data().list;
+          setPaintings(data);
+        }
+        getPaintings();
+      },[]);
+
 
     return(
-        <div>
-            Art
+        <div className={classes.paintings}>
+            {paintings.map(painting => (
+                <div className={classes.divPainting}>
+                    <div className={classes.name}>
+                        <p>{painting.name}</p>  
+                    </div>
+                    <img src={painting.imageUrl} />
+                </div>           
+            ))}
         </div>
     )
 }
 
-export default Art;
+export default withFirebase(Art);
