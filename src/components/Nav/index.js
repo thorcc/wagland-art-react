0aecf056d0785b9a949b46f6e5299d38e78a14ee
thorcc <thorchrcow@gmail.com>
2020-05-18 16:30:03 +0200

    create mobile friendly navbar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { withFirebase } from '../Firebase';
@@ -8,6 +8,9 @@ import * as ROUTES from '../../constants/routes';
 
 import styles from './style.module.css';
 import SignOutButton from '../Admin/SignOutButton';
+import classes from './style.module.css';
+
+const Burger = () => <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 18 18"><path fill="white" d="M2 13.5h14V12H2v1.5zm0-4h14V8H2v1.5zM2 4v1.5h14V4H2z"/></svg>;
 
 const Nav = props => {
     const [user, initialising, error] = useAuthState(props.firebase.auth);
@@ -19,11 +22,18 @@ const Nav = props => {
 }
 
 const NavNonAuth = props => {
+    const [open, setOpen] = useState(false);
+
 
     return(
         <nav className={styles.Nav}>
-            <a className={styles.logo} href="./">RW</a>
-            <ul>
+            <div className={styles.LogoBurger}>
+                <a className={styles.logo} href="./">RW</a>
+                <button className={classes.Burger} onClick={() => setOpen(!open)}>
+                    <Burger />
+                </button>
+            </div>
+            <ul className={open ? classes.OpenNav: classes.ClosedNav}>
                 <li>
                     <NavLink exact={true} to={ROUTES.LANDING} activeClassName={styles.active}>Art</NavLink>
                 </li>
@@ -41,26 +51,36 @@ const NavNonAuth = props => {
     )
 }
 
-const NavAuth = ({ user }) => (
-    <nav className={styles.Nav}>
-        <ul>
-            <li>
-                <NavLink exact={true} to={ROUTES.ADMINART} activeClassName={styles.active}>Art</NavLink>
-            </li>
-            <li>
-                <NavLink exact={true} to={ROUTES.ADMINBIO} activeClassName={styles.active}>Biography</NavLink>
-            </li>
-            <li>
-                <NavLink exact={true} to={ROUTES.ADMINGUESTBOOK} activeClassName={styles.active}>Guest Book</NavLink>
-            </li>
-            <li>
-                <NavLink exact={true} to={ROUTES.ADMINCONTACT} activeClassName={styles.active}>Contact</NavLink>
-            </li>
-            <li className={styles.UserInfo}>
-                <p>{user.email}</p>
-                <SignOutButton />
-            </li>
-        </ul>
-    </nav>
-)
+const NavAuth = ({ user }) => {
+    const [open, setOpen] = useState(false);
+
+    return(
+        <nav className={styles.Nav}>
+            <div className={styles.LogoBurger}>
+                <a className={styles.logo} href="./">RW</a>
+                <button className={classes.Burger} onClick={() => setOpen(!open)}>
+                    <Burger />
+                </button>
+            </div>
+            <ul className={open ? classes.OpenNav: classes.ClosedNav}>
+                <li>
+                    <NavLink exact={true} to={ROUTES.ADMINART} activeClassName={styles.active}>Art</NavLink>
+                </li>
+                <li>
+                    <NavLink exact={true} to={ROUTES.ADMINBIO} activeClassName={styles.active}>Biography</NavLink>
+                </li>
+                <li>
+                    <NavLink exact={true} to={ROUTES.ADMINGUESTBOOK} activeClassName={styles.active}>Guest Book</NavLink>
+                </li>
+                <li>
+                    <NavLink exact={true} to={ROUTES.ADMINCONTACT} activeClassName={styles.active}>Contact</NavLink>
+                </li>
+                <li className={styles.UserInfo}>
+                    <p>{user.email}</p>
+                    <SignOutButton />
+                </li>
+            </ul>
+        </nav>
+    )
+}
 export default withFirebase(Nav);
